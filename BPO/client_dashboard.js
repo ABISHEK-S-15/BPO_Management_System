011b3634d7c149241ed75edee86a2f4855623eb6
ABISHEK-S-15 <146058209+ABISHEK-S-15@users.noreply.github.com>
2024-03-12 06:49:41 +0530

    Add files via upload

---
+++ BPO/client_dashboard.js
@@ -0,0 +1,9 @@
+let popup =document.getElementById("form-container");
+
+    function openpopup(){
+        popup.classList.add("open-popup");
+    }
+    
+    function closepopup(){
+        popup.classList.remove("open-popup");
+    }
