3fa6a51c9872a45863fb84a1460d01c5cf76df54
Barathkumar005 <146067887+Barathkumar005@users.noreply.github.com>
2024-03-13 07:54:37 +0530

    Add files via upload

---
+++ BPO new/dash-form.js
@@ -0,0 +1,9 @@
+let popup =document.getElementById("formcontainer");
+
+    function openpopup(){
+        popup.classList.add("open-popup")
+    }
+    
+    function closepopup(){
+        popup.classList.remove("open-popup")
+    }
