da029d293629f5cfe404602561d26ac8e59a209d
Barathkumar005 <146067887+Barathkumar005@users.noreply.github.com>
2024-03-31 21:37:16 +0530

    Add files via upload

---
+++ Bpo_management_system/db/bpo_management.sql
@@ -0,0 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 31, 2024 at 06:01 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bpo_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `mobile` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `email`, `password`, `mobile`) VALUES
+(1, 'Admin', 'bpoadmin@123', 'admin123', 2147483647);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dashboard`
+--
+
+CREATE TABLE `dashboard` (
+  `id` int(11) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `description` text NOT NULL,
+  `startdate` date NOT NULL,
+  `enddate` date NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `filename` varchar(100) NOT NULL,
+  `folderpath` varchar(100) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mobile` int(12) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `company` text NOT NULL,
+  `position` varchar(20) NOT NULL,
+  `address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dashboard`
+--
+ALTER TABLE `dashboard`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `dashboard`
+--
+ALTER TABLE `dashboard`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Bpo_management_system/payment.php
@@ -0,0 +1,147 @@
+<?php 
+ include('connection.php');
+
+ $msg="";
+  
+if (isset($_POST['submit'])) {
+    header("location:download.php");
+    
+  }
+  else{
+    $msg="Please Enter Valid Details !";
+  }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="payment.css">
+
+</head>
+<body>
+<div class="back-btn-con">
+            <a href="client_dashboard.php"><button class="back-btn">Back</button></a>
+        </div>
+<div class="container">
+
+    <form action="" method="post">
+    <div class="row">
+        <div class="col">
+            <h3 class="title">Billing Address</h3>
+
+            <div class="inputbox">
+                <span>Full Name:</span>
+                <input type="text" placeholder="Enter Name">
+            </div>
+            <div class="inputbox">
+                <span>Email:</span>
+                <input type="email" placeholder="Enter Email">
+            </div>
+            <div class="inputbox">
+                <span>Address:</span>
+                <input type="text" placeholder="Enter Address">
+            </div>
+            <div class="inputbox">
+                <span>City:</span>
+                <input type="text" placeholder="Enter City">
+            </div>
+            <div class="flex">
+                <div class="inputbox">
+                    <span>State:</span>
+                    <input type="text" placeholder="Enter State">
+                </div>
+                <div class="inputbox">
+                    <span>zip Code:</span>
+                    <input type="text" placeholder="Enter Zip Code">
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col">
+            <h3 class="title">Payment</h3>
+            <div class="inputbox">
+                <span>Cards Accepted:</span>
+                <img style="height:200px; width: 200px;" src="images/payment-img.png" alt="">
+            </div>
+            <div class="inputbox">
+                <span>Name on card:</span>
+                <input type="text" placeholder="Enter Name  ">
+            </div>
+            <div class="inputbox">
+                <span>Card Number:</span>
+                <input type="number" placeholder="Enter Number">
+            </div>
+            <div class="inputbox">
+                <span>Address:</span>
+                <input type="text" placeholder="Enter Address">
+            </div>
+            <div class="inputbox">
+                <span>Exp Month:</span>
+                <input type="text" placeholder="Enter Month">
+            </div>
+            <div class="flex">
+                <div class="inputbox">
+                    <span>Exp Year:</span>
+                    <input type="number" placeholder="Enter Year">
+                </div>
+                <div class="inputbox">
+                    <span>CVV:</span>
+                    <input type="text" placeholder="Enter CVV">
+                </div>
+            </div>
+            <div class="flex">
+                <div class="inputbox">
+                <?php
+                session_start();
+                    include('connection.php');
+
+                    if (!isset($_SESSION['USER_ID'])) {
+                      header("location:index.php");
+                      die();
+                    }
+
+
+
+                    $user = $_SESSION['UNSER_NAME'];
+                    $query = mysqli_query($conn,"select * from users where email = '$user'");
+                    $rowr =mysqli_fetch_array($query);
+                    $id = $rowr['id'];
+
+
+                    $query1 = mysqli_query($conn,"select * from dashboard where user_id = '$id'");
+                    $result = mysqli_num_rows($query1);
+
+                ?>
+                <?php 
+                      for($i=1; $i<=$result;$i++)
+                  {
+                      $row =  mysqli_fetch_array($query1)
+                    ?>
+
+
+                    <span>Requested Amount:</span>
+                    <input type="number" placeholder="<?php echo $row['amount']; ?>" disabled>
+                    <?php 
+                    
+                } ?>
+                </div>
+                <div class="inputbox">
+                    <span>Amount:</span>
+                    <input type="number" placeholder="Enter Amount">
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <input type="submit" name="submit" class="submit-btn">
+    </form>
+</div>
+</body>
+</html>
