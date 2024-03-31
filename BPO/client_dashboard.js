6910c131927715a99603d5777c8c91c2296f1747
Barathkumar005 <146067887+Barathkumar005@users.noreply.github.com>
2024-03-31 19:19:13 +0530

    Delete BPO directory

--- BPO/client_dashboard.js
+++
@@ -1,9 +0,0 @@
-let popup =document.getElementById("form-container");
-
-    function openpopup(){
-        popup.classList.add("open-popup");
-    }
-    
-    function closepopup(){
-        popup.classList.remove("open-popup");
-    }
